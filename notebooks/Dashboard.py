29ffd0f3b0e00747a3835b452f303f4056791abc
shreyakabra <kabrashreya23@gmail.com>
2025-05-02 17:20:32 +0530

    Update Streamlit Dashboard and project changes

--- notebooks/Dashboard.py
+++ notebooks/Dashboard.py
@@ -16,6 +16,7 @@ with open("app/style.css") as f:
 
 # Load preprocessed data
 df = pd.read_csv(r'data/feature_engineered_incident_data.csv')
+473ba71 (Update Streamlit Dashboard and project changes)
 
 # Convert 'timestamp' column to datetime
 df['timestamp'] = pd.to_datetime(df['timestamp'])
